31fed0c7245b6e0bb8935a8b4fda9e134ebe0170
Tamaro Skaljic <49238587+tamaro-skaljic@users.noreply.github.com>
2025-05-02 13:29:12 +0200

    remove unnecessary dsl method abstractions from API

--- derive-output/src/api/dsl/method.rs
+++ derive-output/src/api/dsl/method.rs
@@ -4,12 +4,12 @@
 #[cfg_attr(feature = "partial-ord", derive(PartialOrd))]
 #[cfg_attr(feature = "spacetime-type", derive(spacetimedb::SpacetimeType))]
 pub struct TableDSLMethods {
-    pub create_row: CreateRowDSLMethod,
-    pub get_all_rows: GetAllRowsDSLMethod,
-    pub get_count_of_rows: GetCountOfRowsDSLMethod,
+    pub create_row: DSLMethod,
+    pub get_all_rows: DSLMethod,
+    pub get_count_of_rows: DSLMethod,
     // For multi-column indices
-    pub get_many_rows_by: Vec<GetManyRowsByDSLMethod>,
-    pub delete_many_rows_by: Vec<DeleteManyRowsByDSLMethod>,
+    pub get_many_rows_by: Vec<DSLMethod>,
+    pub delete_many_rows_by: Vec<DSLMethod>,
 }
 
 #[cfg_attr(feature = "clone", derive(Clone))]
@@ -17,34 +17,11 @@ pub struct TableDSLMethods {
 #[cfg_attr(feature = "partial-eq", derive(PartialEq))]
 #[cfg_attr(feature = "partial-ord", derive(PartialOrd))]
 #[cfg_attr(feature = "spacetime-type", derive(spacetimedb::SpacetimeType))]
-pub struct CreateRowDSLMethod {
+pub struct DSLMethod {
+    pub doc_comment: Box<str>,
     pub trait_name: Box<str>,
     pub method_name: Box<str>,
     pub method_args: Vec<Box<str>>,
-    pub return_type: Box<str>,
-    pub method_impl: Box<str>,
-}
-
-#[cfg_attr(feature = "clone", derive(Clone))]
-#[cfg_attr(feature = "debug", derive(Debug))]
-#[cfg_attr(feature = "partial-eq", derive(PartialEq))]
-#[cfg_attr(feature = "partial-ord", derive(PartialOrd))]
-#[cfg_attr(feature = "spacetime-type", derive(spacetimedb::SpacetimeType))]
-pub struct GetAllRowsDSLMethod {
-    pub trait_name: Box<str>,
-    pub method_name: Box<str>,
-    pub return_type: Box<str>,
-    pub method_impl: Box<str>,
-}
-
-#[cfg_attr(feature = "clone", derive(Clone))]
-#[cfg_attr(feature = "debug", derive(Debug))]
-#[cfg_attr(feature = "partial-eq", derive(PartialEq))]
-#[cfg_attr(feature = "partial-ord", derive(PartialOrd))]
-#[cfg_attr(feature = "spacetime-type", derive(spacetimedb::SpacetimeType))]
-pub struct GetCountOfRowsDSLMethod {
-    pub trait_name: Box<str>,
-    pub method_name: Box<str>,
     pub return_type: Box<str>,
     pub method_impl: Box<str>,
 }
@@ -65,34 +42,8 @@ pub enum ColumnDSLMethods {
 #[cfg_attr(feature = "partial-ord", derive(PartialOrd))]
 #[cfg_attr(feature = "spacetime-type", derive(spacetimedb::SpacetimeType))]
 pub struct ColumnDSLMethodsForBtreeIndices {
-    pub get_many_rows_by: GetManyRowsByDSLMethod,
-    pub delete_many_rows_by: DeleteManyRowsByDSLMethod,
-}
-
-#[cfg_attr(feature = "clone", derive(Clone))]
-#[cfg_attr(feature = "debug", derive(Debug))]
-#[cfg_attr(feature = "partial-eq", derive(PartialEq))]
-#[cfg_attr(feature = "partial-ord", derive(PartialOrd))]
-#[cfg_attr(feature = "spacetime-type", derive(spacetimedb::SpacetimeType))]
-pub struct GetManyRowsByDSLMethod {
-    pub trait_name: Box<str>,
-    pub method_name: Box<str>,
-    pub method_arg: Box<str>,
-    pub return_type: Box<str>,
-    pub method_impl: Box<str>,
-}
-
-#[cfg_attr(feature = "clone", derive(Clone))]
-#[cfg_attr(feature = "debug", derive(Debug))]
-#[cfg_attr(feature = "partial-eq", derive(PartialEq))]
-#[cfg_attr(feature = "partial-ord", derive(PartialOrd))]
-#[cfg_attr(feature = "spacetime-type", derive(spacetimedb::SpacetimeType))]
-pub struct DeleteManyRowsByDSLMethod {
-    pub trait_name: Box<str>,
-    pub method_name: Box<str>,
-    pub method_arg: Box<str>,
-    pub return_type: Box<str>,
-    pub method_impl: Box<str>,
+    pub get_many_rows_by: DSLMethod,
+    pub delete_many_rows_by: DSLMethod,
 }
 
 #[cfg_attr(feature = "clone", derive(Clone))]
@@ -101,60 +52,8 @@ pub struct DeleteManyRowsByDSLMethod {
 #[cfg_attr(feature = "partial-ord", derive(PartialOrd))]
 #[cfg_attr(feature = "spacetime-type", derive(spacetimedb::SpacetimeType))]
 pub struct ColumnDSLMethodsForUniqueConstraints {
-    pub get_one_row_option_by: GetOneRowOptionByDSLMethod,
-    pub get_many_row_options_by: GetManyRowOptionsByDSLMethod,
-    pub update_row_by: UpdateRowByDSLMethod,
-    pub delete_one_row_by: DeleteOneRowByDSLMethod,
-}
-
-#[cfg_attr(feature = "clone", derive(Clone))]
-#[cfg_attr(feature = "debug", derive(Debug))]
-#[cfg_attr(feature = "partial-eq", derive(PartialEq))]
-#[cfg_attr(feature = "partial-ord", derive(PartialOrd))]
-#[cfg_attr(feature = "spacetime-type", derive(spacetimedb::SpacetimeType))]
-pub struct GetOneRowOptionByDSLMethod {
-    pub trait_name: Box<str>,
-    pub method_name: Box<str>,
-    pub method_arg: Box<str>,
-    pub return_type: Box<str>,
-    pub method_impl: Box<str>,
-}
-
-#[cfg_attr(feature = "clone", derive(Clone))]
-#[cfg_attr(feature = "debug", derive(Debug))]
-#[cfg_attr(feature = "partial-eq", derive(PartialEq))]
-#[cfg_attr(feature = "partial-ord", derive(PartialOrd))]
-#[cfg_attr(feature = "spacetime-type", derive(spacetimedb::SpacetimeType))]
-pub struct GetManyRowOptionsByDSLMethod {
-    pub trait_name: Box<str>,
-    pub method_name: Box<str>,
-    pub method_arg: Box<str>,
-    pub return_type: Box<str>,
-    pub method_impl: Box<str>,
-}
-
-#[cfg_attr(feature = "clone", derive(Clone))]
-#[cfg_attr(feature = "debug", derive(Debug))]
-#[cfg_attr(feature = "partial-eq", derive(PartialEq))]
-#[cfg_attr(feature = "partial-ord", derive(PartialOrd))]
-#[cfg_attr(feature = "spacetime-type", derive(spacetimedb::SpacetimeType))]
-pub struct UpdateRowByDSLMethod {
-    pub trait_name: Box<str>,
-    pub method_name: Box<str>,
-    pub method_arg: Box<str>,
-    pub return_type: Box<str>,
-    pub method_impl: Box<str>,
-}
-
-#[cfg_attr(feature = "clone", derive(Clone))]
-#[cfg_attr(feature = "debug", derive(Debug))]
-#[cfg_attr(feature = "partial-eq", derive(PartialEq))]
-#[cfg_attr(feature = "partial-ord", derive(PartialOrd))]
-#[cfg_attr(feature = "spacetime-type", derive(spacetimedb::SpacetimeType))]
-pub struct DeleteOneRowByDSLMethod {
-    pub trait_name: Box<str>,
-    pub method_name: Box<str>,
-    pub method_arg: Box<str>,
-    pub return_type: Box<str>,
-    pub method_impl: Box<str>,
+    pub get_one_row_option_by: DSLMethod,
+    pub get_many_row_options_by: DSLMethod,
+    pub update_row_by: DSLMethod,
+    pub delete_one_row_by: DSLMethod,
 }
