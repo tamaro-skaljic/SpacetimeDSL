8b6e744ec83c9c484d3906c5456ce01175dcc324
Tamaro Skaljic <49238587+tamaro-skaljic@users.noreply.github.com>
2025-06-24 03:54:09 +0200

    implement DeletionResult and ReferenceIntegrityViolationError

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,10 @@ pub trait Wrapper<WrappedType: Clone + Default, WrapperType> {
     fn value(&self) -> WrappedType;
 }
 
+// TODO: New Feature "Soft Deletion" - if a table has a column "deleted: bool" then there is another dsl method which sets the flag to true instead of deleting the row.
+
 // Don't forget to copy + paste this enum into `derive_input::api::dsl::foreign_key` if you change it
+#[derive(Debug, Eq, PartialEq, Hash)]
 pub enum OnDeleteStrategy {
     /// Available independent from the column type.
     Error,
@@ -43,52 +46,135 @@ pub enum OnDeleteStrategy {
     SetZero,
 }
 
-pub struct ReferenceIntegrityViolationError {
-    pub table: Box<str>,
-    pub value: Box<str>,
-    pub primary_key_column: Box<str>,
-    pub violations: Vec<ReferenceIntegrityViolations>,
+#[derive(Debug)]
+pub struct DeletionResult {
+    pub table_name: Box<str>,
+    pub primary_key_values: Vec<PrimaryKeyValue>,
+    pub on_delete_strategy_executions: Option<OnDeleteStrategyExecutions>,
 }
 
-type ForeignTableName = Box<str>;
-type ForeignColumnName = Box<str>;
-type ForeignRowPrimaryKeyValue = Box<str>;
-type ReferenceIntegrityViolations = HashMap<ForeignTableName, HashMap<ForeignColumnName, Vec<ForeignRowPrimaryKeyValue>>>;
+impl DeletionResult {
+    pub fn is_one_deletion(&self) -> bool {
+        self.primary_key_values.len().eq(&1)
+    }
+}
+
+pub type OnDeleteStrategyExecutions =
+    HashMap<PrimaryKeyValue, HashMap<OnDeleteStrategy, Vec<(ColumnName, DeletionResult)>>>;
+
+pub type ReferenceIntegrityViolationError = DeletionResult;
+
+type ColumnName = Box<str>;
+type PrimaryKeyValue = Box<str>;
 
 #[doc(hidden)]
 pub mod internal {
-    use crate::ReferenceIntegrityViolationError;
+    use crate::{DeletionResult, OnDeleteStrategy, ReferenceIntegrityViolationError};
     use core::fmt;
 
     pub struct DSLInternals;
 
     impl fmt::Display for ReferenceIntegrityViolationError {
         fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-            let mut error_message: String = String::new();
+            let mut message: String = String::new();
 
-            error_message.push_str(&format!(
-                "deletion error on table `{}`: The value `{}` in the primary key column `{}`...",
-                self.table, self.value, self.primary_key_column
+            message.push_str("Reference Integrity Violation Error: While deleting ");
+
+            let only_one_deletion = self.is_one_deletion();
+            if only_one_deletion {
+                message.push_str("a row ");
+            } else {
+                message.push_str("rows ");
+            }
+
+            message.push_str(&format!(
+                "in the `{}` table. The following reference integrity violations occurred:\n\n",
+                &self.table_name
             ));
 
-            for violation in self.violations.iter() {
-                for (foreign_table_name, foreign_row_primary_key_values_by_foreign_column_name) in
-                    violation.iter()
-                {
-                    error_message.push_str(&format!(
-                        "\n... is referenced in the table {} by...\n",
-                        foreign_table_name
-                    ));
+            message.push_str("error_id,table,pk_value,parent_error_id,reason,trace\n");
 
-                    for (foreign_column_name, foreign_row_primary_key_values) in
-                        foreign_row_primary_key_values_by_foreign_column_name.iter()
-                    {
-                        error_message.push_str(&format!("... the foreign key on the column `{}` with `on_delete = Error`. Found:\n{:?}\n", foreign_column_name, foreign_row_primary_key_values));
-                    }
+            let mut error_id: u128 = 1;
+            let mut parent_error_id: Box<str> = "".into();
+
+            for (primary_key_value, results_by_strategy) in
+                self.on_delete_strategy_executions.as_ref().unwrap()
+            {
+                message.push_str(&format!(
+                    "{},{},{},{},,\n",
+                    &error_id, &self.table_name, &primary_key_value, &parent_error_id
+                ));
+
+                parent_error_id = format!("{error_id}").into();
+                let trace = format!("{error_id}").into();
+
+                for (foreign_key_column_name, result) in
+                    results_by_strategy.get(&OnDeleteStrategy::Error).unwrap()
+                {
+                    (message, error_id) = process_each_reference_integrity_violation(
+                        message,
+                        error_id,
+                        &parent_error_id,
+                        foreign_key_column_name,
+                        primary_key_value,
+                        &trace,
+                        result,
+                    )
                 }
             }
 
-            write!(f, "{}", error_message)
+            write!(f, "{}", message)
         }
+    }
+
+    fn process_each_reference_integrity_violation(
+        mut message: String,
+        mut error_id: u128,
+        parent_error_id: &Box<str>,
+        foreign_key_column_name: &Box<str>,
+        foreign_key_column_value: &Box<str>,
+        trace: &Box<str>,
+        result: &DeletionResult,
+    ) -> (String, u128) {
+        for primary_key_value in &result.primary_key_values {
+            error_id += 1;
+
+            let trace: Box<str> = format!("{trace}<-{error_id}").into();
+            message.push_str(&format!(
+                "{},{},{},{},{} == {},{}{}\n",
+                &error_id,
+                &result.table_name,
+                &primary_key_value,
+                &parent_error_id,
+                foreign_key_column_name,
+                foreign_key_column_value,
+                trace,
+                &error_id
+            ));
+
+            if result.on_delete_strategy_executions.is_some() {
+                for (foreign_key_column_name, result) in result
+                    .on_delete_strategy_executions
+                    .as_ref()
+                    .unwrap()
+                    .get(primary_key_value)
+                    .unwrap()
+                    .get(&OnDeleteStrategy::Error)
+                    .unwrap()
+                {
+                    (message, error_id) = process_each_reference_integrity_violation(
+                        message,
+                        error_id,
+                        &parent_error_id,
+                        foreign_key_column_name,
+                        &primary_key_value,
+                        &trace,
+                        result,
+                    )
+                }
+            }
+        }
+
+        (message, error_id)
     }
 }
