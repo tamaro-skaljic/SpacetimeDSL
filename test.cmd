f093f8269798c8fd2813b4dcd95a309cfa21260c
Tamaro Skaljic <49238587+tamaro-skaljic@users.noreply.github.com>
2025-07-16 04:10:57 +0200

    move debug commands in test.cmd to debug.cmd

--- test.cmd
+++ test.cmd
@@ -21,19 +21,3 @@ echo.
 spacetime delete spacetimedsl
 echo.
 
-echo Creating Debug Output ...
-echo.
-
-echo Expanding example library ...
-echo.
-cargo expand --lib > ..\debug-helper\output\lib.expanded.rs
-
-cd ..\debug-helper
-echo Creating AST of initial example lib.
-echo.
-cargo run -- ..\example\src\lib.rs > output\lib.rs.ast
-echo.
-
-echo Creating AST of expanded example lib.
-echo.
-cargo run -- output/lib.expanded.rs > output/lib.expanded.rs.ast
