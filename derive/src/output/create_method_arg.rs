0b070c6da876f8db2a614db3375f470c2c1f41a5
Tamaro Skaljic <49238587+tamaro-skaljic@users.noreply.github.com>
2025-10-28 20:27:38 +0100

    Add implementation doc comments

--- derive/src/output/create_method_arg.rs
+++ derive/src/output/create_method_arg.rs
@@ -1,8 +1,24 @@
 use proc_macro2::TokenStream;
 use quote::quote;
+use rust_format::{Formatter, PrettyPlease};
 
 pub fn build(create_dsl_method_arg: &TokenStream) -> syn::Result<TokenStream> {
+    let doc_comment = add_impl_doc(create_dsl_method_arg);
+
     Ok(quote! {
+        #[doc=#doc_comment]
         #create_dsl_method_arg
     })
+}
+
+fn add_impl_doc(create_dsl_method_arg: &TokenStream) -> String {
+    let pretty_please = PrettyPlease::default();
+
+    let implementation_docs = pretty_please
+        .format_tokens(create_dsl_method_arg.clone())
+        .unwrap_or_else(|_| panic!("implementation doc formatting should work - got:\n\n```no_run\n{create_dsl_method_arg}\n```"));
+
+    let doc_comment = format!("\n\nImplementation:\n\n```no_run\n{implementation_docs}\n```",);
+
+    doc_comment
 }
