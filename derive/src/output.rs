09ba4da40684053671edab79b0a60929bc4aaffb
Tamaro Skaljic <49238587+tamaro-skaljic@users.noreply.github.com>
2025-10-29 20:08:53 +0100

    Refactor method argument mapping and improve error reporting

--- derive/src/output.rs
+++ derive/src/output.rs
@@ -2,7 +2,7 @@ use proc_macro2::TokenStream;
 use quote::{format_ident, quote};
 use spacetimedsl_derive_input::api::{
     Table,
-    dsl::{column::SpacetimeDSLColumnMethods, wrapper::WrapperType},
+    dsl::{column::SpacetimeDSLColumnMethods, method::SpacetimeDSLArg, wrapper::WrapperType},
 };
 
 mod accessor;
@@ -139,4 +139,53 @@ fn get_column_dsl_methods(methods: &SpacetimeDSLColumnMethods) -> syn::Result<To
     Ok(quote! {
         #(#token_streams)*
     })
+}
+
+pub fn malformed_code_generation_result(result: String) -> String {
+    let mut result = result.replace("\n", "");
+
+    for _ in 0..20 {
+        result = result.replace("  ", " ");
+    }
+
+    format!("
+
+Congratulations, you have found a bug in SpacetimeDSL!
+
+We would be very pleased if you can create an issue in our GitHub repository: https://github.com/tamaro-skaljic/SpacetimeDSL/issues/new
+
+Please include your table definition as well as the following, malformed, code generation result - thank you very much!
+
+{result}
+
+")
+}
+
+fn map_args(args: &Vec<SpacetimeDSLArg>) -> Vec<TokenStream> {
+    let mut function_args = vec![];
+
+    for arg in args {
+        let arg_name = &arg.arg_name;
+        let arg_type = match &arg.arg_type {
+            spacetimedsl_derive_input::api::dsl::method::SpacetimeDSLArgType::Normal(
+                actual_type,
+            ) => actual_type,
+            spacetimedsl_derive_input::api::dsl::method::SpacetimeDSLArgType::Wrapped {
+                wrapped_type: _,
+                actual_type,
+            } => actual_type,
+        };
+
+        if arg.is_mut {
+            function_args.push(quote! {
+                mut #arg_name: #arg_type
+            });
+        } else {
+            function_args.push(quote! {
+                #arg_name: #arg_type
+            });
+        }
+    }
+
+    function_args
 }
