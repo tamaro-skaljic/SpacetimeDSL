fec0a88aa047268683e34301305d8821bc126d77
Tamaro Skaljic <49238587+tamaro-skaljic@users.noreply.github.com>
2025-12-28 21:51:55 +0100

    Create Linux Tooling and add Tooling Generator (#106)
    * Create Linux Tolling
    
    * Merge Windows Tooling to one script
    
    * Add --server local option to spacetime commands in module build process
    
    * Refactor format command to streamline clippy execution paths
    
    * Add generation script for bash and PowerShell with command handling
    
    * Add header comment to generated scripts indicating they should not be modified directly

--- debug.ps1
+++
@@ -1,5 +0,0 @@
-cd examples\test
-set RUSTFLAGS="-Zmacro-backtrace"; cargo +nightly expand > ..\..\debug-helper\output\lib.expanded.rs
-cd ..\..\debug-helper
-cargo run -- ..\examples\test\src\lib.rs > output\lib.rs.ast
-cd ..
