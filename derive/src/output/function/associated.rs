050ca8b1a3105cd621c925e0e5ddc9460067a904
Nxllpointer <54677650+Nxllpointer@users.noreply.github.com>
2026-03-17 17:32:36 +0100

    Add support for `#[spacetimedb::view]`s (#120)
    * Implement associated method traits for ReadContext
    
    * Clean up code duplication
    
    * Clippy
    
    * Make test example project use spacetimedsl for views
    
    * Remove update todo comment
    
    * Rename trait_dep_paths to additional_paths_to_use
    
    * Add documentation to readme

--- derive/src/output/function/associated.rs
+++ derive/src/output/function/associated.rs
@@ -1,2 +1,85 @@
-pub mod with_lifetime;
-pub mod without_lifetime;
+use proc_macro2::TokenStream;
+use quote::quote;
+use rust_format::{Formatter, PrettyPlease};
+use spacetimedsl_derive_input::api::dsl::method::SpacetimeDSLMethod;
+
+use crate::output::malformed_code_generation_result;
+
+fn build_impl(
+    method: &SpacetimeDSLMethod,
+    method_args: &[TokenStream],
+    context_bound: TokenStream,
+    dsl_type: TokenStream,
+    context_import: TokenStream,
+) -> TokenStream {
+    let trait_name = &method.trait_name;
+    let additional_paths_to_use = &method.additional_paths_to_use;
+    let method_name = &method.method_name;
+    let return_type = &method.return_type;
+    let method_impl = &method.method_impl;
+
+    quote! {
+        impl<T: #context_bound> #trait_name for #dsl_type {
+            #[allow(clippy::needless_lifetimes, clippy::too_many_arguments)]
+            fn #method_name<'a>(
+                &'a self,
+                #(#method_args),*
+            ) -> #return_type {
+                use spacetimedsl::Wrapper;
+                use spacetimedb::{DbContext, Table as _};
+                use #context_import;
+                #(use #additional_paths_to_use as _;)*
+                #method_impl
+            }
+        }
+    }
+}
+
+pub fn build(method: &SpacetimeDSLMethod) -> syn::Result<TokenStream> {
+    let trait_name = &method.trait_name;
+    let method_name = &method.method_name;
+    let method_args = crate::output::map_args(&method.method_args);
+    let return_type = &method.return_type;
+
+    let mut dsl_impl = build_impl(
+        method,
+        &method_args,
+        quote! { spacetimedsl::WriteContext },
+        quote! { spacetimedsl::DSL<'_, T> },
+        quote! { spacetimedsl::DSLContext },
+    );
+
+    if method.read_context_compatible {
+        dsl_impl.extend(build_impl(
+            method,
+            &method_args,
+            quote! { spacetimedsl::ReadContext },
+            quote! { spacetimedsl::ReadOnlyDSL<'_, T> },
+            quote! { spacetimedsl::ReadOnlyDSLContext },
+        ));
+    }
+
+    let impl_doc = PrettyPlease::default()
+        .format_tokens(dsl_impl.clone())
+        .unwrap_or_else(|_| panic!("{}", malformed_code_generation_result(dsl_impl.to_string())));
+
+    let doc_comment = format!(
+        "{}\n\nImplementation:\n\n```no_run\n{impl_doc}\n```",
+        method.doc_comment
+    );
+    let trait_comment = format!("See [`Self::{method_name}`] for details.");
+
+    Ok(quote! {
+        #[doc = #trait_comment]
+        pub trait #trait_name {
+            #[allow(clippy::needless_lifetimes, clippy::too_many_arguments)]
+            #[doc=#doc_comment]
+            fn #method_name<'a>(
+                &'a self,
+                #(#method_args),*
+            ) -> #return_type;
+        }
+
+        #dsl_impl
+    })
+}
